74836a5fac1f7b2706ff1041c7a4fc760f57f708
Shashwat Anand <22ucs193@lnmiit.ac.in>
2023-11-04 13:31:07 +0530

    removed route

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Navbar />
         <Home />
         <Routes>
-          <Route exact path='/' element={<Home />} />
+          {/* <Route exact path='/' element={<Home />} /> */}
         </Routes>
       </Router>
     </>
